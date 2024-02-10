de84eb6f54588bd064b528ca45cb6664bb833639
Mifastara <158732447+Mifastara@users.noreply.github.com>
2024-02-10 19:02:31 +0300

    AI:1

---
+++ Assets/script/EnemyAi.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAi : MonoBehaviour
+{
+    public Transform targetPoint;
+    private NavMeshAgent _navMeshAgent;
+
+    void Start()
+    {
+        _navMeshAgent = GetComponent<NavMeshAgent>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _navMeshAgent.destination = targetPoint.position;
+    }
+}
